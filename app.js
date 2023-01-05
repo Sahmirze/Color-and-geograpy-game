7a8da28ee5af02d6f62bcbd42aabe1353ca8e416
Shahmirze <sahmirze.memmedyarov@gmail.com>
2023-01-05 23:55:48 +0400

    all problem is over and Finished

--- app.js
+++ app.js
@@ -5,68 +5,8 @@ const clr = document.querySelector('.clr')
 const click = document.querySelector('.click')
 const testID = document.getElementById("testID")
 const checkBtn = document.getElementById("checkBtn")
-let colorArr = ['red',
-    'lime',
-    'black',
-    'purple',
-    'blue',
-    'orange',
-    'aqua',
-    'yellow',
-    'beige',
-    'brown',
-    'teal',
-    'gold'
-]
+const headerP = document.querySelector('header>p')
 
-let question = [{
-        country: "Norway",
-        capital: "Oslo"
-    },
-    {
-        country: "Netherlands",
-        capital: "Amsterdam"
-    }, {
-        country: "Belgium",
-        capital: "Brussels"
-    },
-    {
-        country: "Latvia",
-        capital: "Riga"
-    }, {
-        country: "Estonia",
-        capital: "Tallinn"
-    }, {
-        country: "Lithuanian",
-        capital: "Vilnius"
-    }, {
-        country: " Denmark",
-        capital: "Copenhagen"
-    },
-    {
-        country: "Czechia",
-        capital: "Prague"
-    },
-    {
-        country: "Poland",
-        capital: "Warsaw"
-    },
-    {
-        country: "Finland",
-        capital: "Helsinki"
-    },
-    {
-        country: "Luxembourg",
-        capital: "Luxembourg"
-    },
-    {
-        country: "Montenegro",
-        capital: "Podgorica"
-    }, {
-        country: "Denmark",
-        capital: "Copenhagen"
-    }
-]
 
 let clrRandom = Math.floor(Math.random() * colorArr.length)
 clr.style.backgroundColor = ` ${ colorArr[clrRandom] }`
@@ -79,44 +19,64 @@ function check() {
     click.style.display = "none"
     testID.style.display = "none"
     checkBtn.style.display = "none"
+
 }
 btn.addEventListener('click', () => {
+
     if (color.value == colorArr[clrRandom]) {
         x++
-        console.log(x)
+
     } else if (x == -1) {
+        countryRandom = Math.floor(Math.random() * question.length)
+        console.log(question[countryRandom].country)
         click.style.display = "block"
         testID.style.display = "block"
         checkBtn.style.display = "block"
         color.disabled = true
+        btn.disabled = true
         if (confirm("Bro Uduzdun Amma davam etmek isteyirsense seni nezere ala bilerem eger Isteyirsense Ok klikle") == true) {
             // x = 0
             click.textContent = `Which is ${ question[countryRandom].country} capital?`
             console.log(question[countryRandom].capital)
             checkBtn.addEventListener('click', () => {
-                if (testID.value == question[countryRandom].capital && testID.value != '') {
+                if (testID.value.toLocaleLowerCase() == question[countryRandom].capital.toLocaleLowerCase() && testID.value != '') {
                     check()
                     color.disabled = false
+                    btn.disabled = false
                     x = 0
-                    countryRandom = Math.floor(Math.random() * question.length)
+                    headerP.textContent = x
+
                 } else {
                     check()
                     color.style.display = "none"
                     btn.style.display = "none"
                     clr.style.display = "none"
                     alert("bro oyunu uduzdun yeniden basla")
+                    color.disabled = true
                 }
+
+
             })
 
-        } else {
-            alert("hey dostum oyunu uduzdun")
-            color.disabled = true
         }
+
     } else {
         x--
         console.log(x)
     }
+    if (x == 5) {
+
+        check()
+        color.style.display = "none"
+        btn.style.display = "none"
+        clr.style.display = "none"
+        headerP.style.display = "none"
+        alert("winer winner chicken dinner")
+    }
+    headerP.textContent = x
     clrRandom = Math.floor(Math.random() * colorArr.length)
     clr.style.backgroundColor = `${colorArr[clrRandom]}`
+        // countryRandom = Math.floor(Math.random() * question.length)
+
 
 })
---
+++ country.js
@@ -0,0 +1,61 @@
+let question = [{
+        country: "Norway",
+        capital: "Oslo"
+    },
+    {
+        country: "Netherlands",
+        capital: "Amsterdam"
+    }, {
+        country: "Belgium",
+        capital: "Brussels"
+    },
+    {
+        country: "Latvia",
+        capital: "Riga"
+    }, {
+        country: "Estonia",
+        capital: "Tallinn"
+    }, {
+        country: "Lithuanian",
+        capital: "Vilnius"
+    }, {
+        country: " Denmark",
+        capital: "Copenhagen"
+    },
+    {
+        country: "Czechia",
+        capital: "Prague"
+    },
+    {
+        country: "Poland",
+        capital: "Warsaw"
+    },
+    {
+        country: "Finland",
+        capital: "Helsinki"
+    },
+    {
+        country: "Luxembourg",
+        capital: "Luxembourg"
+    },
+    {
+        country: "Montenegro",
+        capital: "Podgorica"
+    }, {
+        country: "Denmark",
+        capital: "Copenhagen"
+    }
+]
+let colorArr = ['red',
+    'lime',
+    'black',
+    'purple',
+    'blue',
+    'orange',
+    'aqua',
+    'yellow',
+    'beige',
+    'brown',
+    'teal',
+    'gold'
+]
